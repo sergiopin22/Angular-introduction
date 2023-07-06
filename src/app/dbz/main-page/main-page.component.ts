2e8a41018f6574bbcd93f527fe3d23e511a4c150
Sergio Pinzon <Sergiopin2220@gmail.com>
2023-07-06 15:34:42 -0500

    subiendo ejercicio

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -20,21 +20,9 @@ export class MainPageComponent {
     }
   ];
 
-  nuevo: Personaje= { 
-    nombre: '',
-    poder: 0
-  }
-
-  agregar(){
-    if(this.nuevo.nombre.trim().length === 0){return;}
-
-    console.log(this.nuevo)
-    this.personajes.push(this.nuevo);
-    this.nuevo = {
-      nombre: '',
-      poder: 0
-    }
-    
-  }
+   nuevo: Personaje = {
+    nombre: 'Maestro Roshi',
+    poder: 1000
+   }
 
 }
